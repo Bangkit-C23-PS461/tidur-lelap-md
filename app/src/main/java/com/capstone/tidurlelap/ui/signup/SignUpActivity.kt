1dc0dc2f720d18e288ac016644c333db00f34774
fitriaulias <fitriauliasanni@gmail.com>
2023-05-30 22:59:23 +0700

    commit to fitri's space

---
+++ app/src/main/java/com/capstone/tidurlelap/ui/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.tidurlelap.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.tidurlelap.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
---
+++ app/src/main/java/com/capstone/tidurlelap/ui/tracking/TrackingActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.tidurlelap.ui.tracking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.tidurlelap.R
+
+class TrackingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tracking)
+    }
+}
