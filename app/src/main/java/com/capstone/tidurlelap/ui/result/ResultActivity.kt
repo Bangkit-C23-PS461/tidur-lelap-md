4f2f29a0b2813115d719fbdbd83a7779b9a73db8
ariq028 <ariq028@gmail.com>
2023-06-05 23:20:48 +0700

    Commit XML ariq

---
+++ app/src/main/java/com/capstone/tidurlelap/ui/result/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.tidurlelap.ui.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.tidurlelap.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
