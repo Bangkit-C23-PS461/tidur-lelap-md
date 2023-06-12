aa0ab16c1e6ae140a284760990f813f0e47364a8
ariq028 <ariq028@gmail.com>
2023-06-12 20:17:50 +0700

    Commit Username dari SleepTracking

---
+++ app/src/main/java/com/capstone/tidurlelap/data/remote/response/UserResponse.kt
@@ -0,0 +1,11 @@
+package com.capstone.tidurlelap.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class UserResponse (
+    @field:SerializedName("email")
+    val email : String,
+
+    @field:SerializedName("username")
+    val username : String
+        )
